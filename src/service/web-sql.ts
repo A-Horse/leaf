842aa4af3aaa3e883971916358bc5157285dfd4f
fwchen <crystal.tyan@gmail.com>
2017-03-06 23:20:12 +0800

    initial commit

--- src/service/web-sql.ts
+++ src/service/web-sql.ts
@@ -4,5 +4,14 @@ export class WebSql {
 
   }
 
+  openDb(name, version, description, size = 5 * 1024 * 1024) {
+    this.db = openDatabase(name, version, description, size);
+  }
+
+  execSQL(sql) {
+    this.db.transaction((tx) => {
+      tx.executeSql(sql, []);
+    });
+  }
 
 }
