f0ad96aa1bb49ace80241c0d3a7bbe08f21d4f8c
fwchen <crystal.tyan@gmail.com>
2017-03-13 21:58:13 +0800

    update

--- main.js
+++ main.js
@@ -8,7 +8,11 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600})
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    'web-preferences': {'web-security': false}
+  })
 
   // and load the index.html of the app.
   win.loadURL(url.format({
